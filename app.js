1e04152c4599ffc8716f08e3f217a0332e32cad0
eladsadeh <elad.sadeh1@.gmail.com>
2022-08-07 11:55:41 -0400

    add endGame function and refracture

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 import texts from './text.json' assert {type: 'json'};
 
-let timeCounter = 0;
-let isTimerRunning = 0;
-
 const selectText = () => {
   const index = Math.floor(Math.random()*texts.length)
   return texts[index]
@@ -10,26 +7,14 @@ const selectText = () => {
 
 const countWords = (text) => {
   return text[text.length-1] === ' ' ? text.trim().split(/[\s-]/).length :
-  text.trim().split(/[\s-]/).length-1};
-
-const compareText = (input,ref) => {
-  let complete = input.length === ref.length;
-  let errors = 0;
-  if (input !== ref) {
-    for (let i = 0; i < input.length; i++) {
-      if (input[i] !== ref[i]) errors++;
-    }
-  }
-  return {errors, complete}
-}
+  text.trim().split(/[\s-]/).length-1
+};
 
 function secondsToString(time) {
 	const minutes = Math.floor(time / 60);
 	const seconds = minutes ? time % 60 : time;
 	return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
-		2,
-		'0'
-	)}`;
+		2, '0')}`;
 }
 
 function runTimer(run) {
@@ -44,18 +29,43 @@ function runTimer(run) {
 	}
 }
 
+const processText = (input,ref) => {
+  //start timer if not running already
+  if (!isTimerRunning) runTimer(true);
+  // check and count errors
+  let errors = 0;
+  if (input !== ref) {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] !== ref[i]) errors++;
+    }
+  }
+  errors ? inputTextEl.classList.add('error') : inputTextEl.classList.remove('error');
+}
+
+function endGame(text,ref) {
+  console.log('ending game');
+  // stop timer
+  runTimer(false)
+  // disable typing
+  // calculate and show WPM
+  // Show errors in input text
+  // Show number/precentage of errors
+  // add/change button to start new test
+
+}
+
 const handleInput = (ev) => {
   ev.preventDefault();
   const inputText = ev.target.value
-  const {errors, complete} = compareText(inputText,currentText);
-  errors ? inputTextEl.classList.add('error') : inputTextEl.classList.remove('error');
-
-  if (!isTimerRunning && !complete) runTimer(true);
-  else if (isTimerRunning && complete) runTimer(false);
+  if (inputText.length === currentText.length) endGame(inputText, currentText);
+  else processText(inputText,currentText);
   // console.log('count:', countWords(inputText));
   wordsEl.innerHTML = countWords(inputText);
 
 }
+
+let timeCounter = 0;
+let isTimerRunning = 0;
 
 const timerEl = document.getElementById("timer");
 timerEl.innerHTML = secondsToString(timeCounter)
