fc5d1959ec43559c53f482522cf56b8274140ee0
eladsadeh <elad.sadeh1@.gmail.com>
2022-08-07 17:16:04 -0400

    show errors upon end of test

--- app.js
+++ app.js
@@ -16,28 +16,46 @@ function secondsToString(time) {
 		2, '0')}`;
 }
 
-function mistakesPercentage(input, ref) {
+function calcAccuracy(input, ref) {
+  const total = input.length;
+  if (total === 0) return { errors: 0, accuracy: 100}
   const errors = input.split('').reduce((count, char, i) => char === ref[i]? count: count + 1,0);
-  console.log('errors:',errors);
+  const accuracy = 100*(total - errors) / total;
+  return {errors, accuracy}
 }
 
+function showErrors(input,ref) {
+  let parser = new DOMParser();
+  let html = '<div>';
+  const errorsText = document.createElement('div');
+  errorsText.classList.add("text")
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === ref[i]) errorsText.insertAdjacentHTML('beforeend', input[i])
+    else errorsText.insertAdjacentHTML('beforeend', `<span class="mistake">${input[i]}</span>`)
+    // else html += `<span class="mistake">${input[i]}</span>`
+  }
+  html += '</div>'
+  // console.log('html:', html)
+  // console.log(parser.parseFromString(html, 'text/html').body);
+  return errorsText;
+  
+}
 function runTimer(run) {
 	if (run) {
 		isTimerRunning = setInterval(() => {
 			timerEl.innerHTML = secondsToString(++timeCounter);
 		}, 1000);
 	} else {
-		// Pause - clear the setInterval
 		clearInterval(isTimerRunning);
 		isTimerRunning = 0;
 	}
 }
 
-function showWordsPerMinutes(text) {
+function wordsPerMinutes(text) {
   const wpm = timeCounter
     ? countWords(text)/(timeCounter/60)
     : 0
-  document.getElementById("wpm").innerText = wpm.toFixed(1); 
+  return wpm;
 }
 
 const processText = (input,ref) => {
@@ -47,51 +65,52 @@ const processText = (input,ref) => {
   ref.slice(0,input.length) !== input 
     ? inputTextEl.classList.add('error') 
     : inputTextEl.classList.remove('error');
-  wordsEl.innerHTML = countWords(input);
-  showWordsPerMinutes(input)
-  mistakesPercentage(input,ref)
-  // const wpm = countWords(input)/(timeCounter/60);
-  // document.getElementById("wpm").innerText = wpm;
+  wordsEl.innerHTML = `${countWords(input)} / ${wordsCount}`;
+  wpmEl.innerText = wordsPerMinutes(input).toFixed(1); 
+  const {errors, accuracy} = calcAccuracy(input,ref);
+  accuDetailEl.innerText = `${input.length - errors} / ${input.length}`;
+  accuracyEl.innerText = `${Math.round(accuracy)}%`;
 }
 
 function endGame(text,ref) {
-  console.log('ending game');
+  messageEl.innerText = "Test Ended"
   // stop timer
   runTimer(false)
   // disable typing
   inputTextEl.removeEventListener('input',handleInput);
-  document.getElementById("input").readOnly = true;
-  // calculate and show WPM
-  showWordsPerMinutes(text)
-  // const wpm = countWords(text)/(timeCounter/60);
-  // document.getElementById("wpm").innerText = wpm;
-  // Show errors in input text
-  // Show number/precentage of errors
-  // add/change button to start new test
+  const testResultsEl = showErrors(text,ref)
+  inputTextEl.remove();
+  document.getElementById('texts').appendChild(testResultsEl)
+  inputTextEl.innerHTML = result;
 
 }
 
 const handleInput = (ev) => {
   ev.preventDefault();
   const inputText = ev.target.value
+  processText(inputText,currentText);
   if (inputText.length === currentText.length) endGame(inputText, currentText);
-  else processText(inputText,currentText);
-  // console.log('count:', countWords(inputText));
-
 }
 
+const currentText = selectText();
+const wordsCount = countWords(currentText);
 let timeCounter = 0;
 let isTimerRunning = 0;
-showWordsPerMinutes("")
+// showWordsPerMinutes("")
 const timerEl = document.getElementById("timer");
-timerEl.innerHTML = secondsToString(timeCounter)
 const wordsEl = document.getElementById("words");
-wordsEl.innerHTML = 0;
 const messageEl = document.getElementById("message");
-messageEl.innerHTML = 'Start typing to begin';
-const sampleTextEl = document.getElementById("test-text");
-const currentText = selectText()
+const wpmEl = document.getElementById("wpm");
+const accuracyEl = document.getElementById("accuracy");
+const accuDetailEl = document.getElementById("accuracy-detail");
 const inputTextEl = document.getElementById("input");
-sampleTextEl.innerText = currentText;
+
+timerEl.innerText = secondsToString(timeCounter)
+wordsEl.innerText = `0 / ${wordsCount}`;
+accuDetailEl.innerText = `0 / ${wordsCount}`;
+wpmEl.innerText = '0.0';
+accuracyEl.innerText = '100%'
+messageEl.innerText = 'Start typing to begin';
+document.getElementById("test-text").innerText = currentText;
 
 inputTextEl.addEventListener('input',handleInput);
