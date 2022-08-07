f1df7b472fea38a3969ae796ae7b64c4a44ced75
eladsadeh <elad.sadeh1@.gmail.com>
2022-08-07 18:20:22 -0400

    enclose all variables in runGame function

--- app.js
+++ app.js
@@ -25,92 +25,91 @@ function calcAccuracy(input, ref) {
 }
 
 function showErrors(input,ref) {
-  let parser = new DOMParser();
-  let html = '<div>';
-  const errorsText = document.createElement('div');
-  errorsText.classList.add("text")
+
+  const element = document.createElement('div');
+  element.classList.add("text")
   for (let i = 0; i < input.length; i++) {
-    if (input[i] === ref[i]) errorsText.insertAdjacentHTML('beforeend', input[i])
-    else errorsText.insertAdjacentHTML('beforeend', `<span class="mistake">${input[i]}</span>`)
-    // else html += `<span class="mistake">${input[i]}</span>`
+    if (input[i] === ref[i]) element.insertAdjacentHTML('beforeend', input[i])
+    else element.insertAdjacentHTML('beforeend', `<span class="mistake">${input[i]}</span>`);
   }
-  html += '</div>'
-  // console.log('html:', html)
-  // console.log(parser.parseFromString(html, 'text/html').body);
-  return errorsText;
-  
-}
-function runTimer(run) {
-	if (run) {
-		isTimerRunning = setInterval(() => {
-			timerEl.innerHTML = secondsToString(++timeCounter);
-		}, 1000);
-	} else {
-		clearInterval(isTimerRunning);
-		isTimerRunning = 0;
-	}
+
+  return element;
 }
 
-function wordsPerMinutes(text) {
-  const wpm = timeCounter
-    ? countWords(text)/(timeCounter/60)
-    : 0
-  return wpm;
+function runTest() {
+  function runTimer(run) {
+    if (run) {
+      isTimerRunning = setInterval(() => {
+        timerEl.innerHTML = secondsToString(++timeCounter);
+      }, 1000);
+    } else {
+      clearInterval(isTimerRunning);
+      isTimerRunning = 0;
+    }
+  }
+
+  function wordsPerMinutes(text) {
+    const wpm = timeCounter
+      ? countWords(text)/(timeCounter/60)
+      : 0
+    return wpm;
+  }
+
+  const processText = (input,ref) => {
+    //start timer if not running already
+    if (!isTimerRunning) runTimer(true);
+    // check for errors and change background
+    ref.slice(0,input.length) !== input 
+      ? inputTextEl.classList.add('error') 
+      : inputTextEl.classList.remove('error');
+    wordsEl.innerText = `${countWords(input)} / ${wordsCount}`;
+    wpmEl.innerText = wordsPerMinutes(input).toFixed(1); 
+    const {errors, accuracy} = calcAccuracy(input,ref);
+    accuDetailEl.innerText = `${input.length - errors} / ${input.length}`;
+    accuracyEl.innerText = `${Math.round(accuracy)}%`;
+  }
+
+  function endGame(text,ref) {
+    messageEl.innerText = "Test Ended"
+    // stop timer
+    runTimer(false)
+    // disable typing
+    inputTextEl.removeEventListener('input',handleInput);
+    const testResultsEl = showErrors(text,ref)
+    inputTextEl.remove();
+    document.getElementById('texts').appendChild(testResultsEl)
+    inputTextEl.innerHTML = result;
+
+  }
+
+  const handleInput = (ev) => {
+    ev.preventDefault();
+    const inputText = ev.target.value
+    processText(inputText,currentText);
+    if (inputText.length === currentText.length) endGame(inputText, currentText);
+  }
+
+  const currentText = selectText();
+  const wordsCount = countWords(currentText);
+  let timeCounter = 0;
+  let isTimerRunning = 0;
+  // showWordsPerMinutes("")
+  const timerEl = document.getElementById("timer");
+  const wordsEl = document.getElementById("words");
+  const messageEl = document.getElementById("message");
+  const wpmEl = document.getElementById("wpm");
+  const accuracyEl = document.getElementById("accuracy");
+  const accuDetailEl = document.getElementById("accuracy-detail");
+  const inputTextEl = document.getElementById("input");
+
+  timerEl.innerText = secondsToString(timeCounter)
+  wordsEl.innerText = `0 / ${wordsCount}`;
+  accuDetailEl.innerText = `0 / 0`;
+  wpmEl.innerText = '0.0';
+  accuracyEl.innerText = '100%'
+  messageEl.innerText = 'Start typing to begin';
+  document.getElementById("test-text").innerText = currentText;
+  inputTextEl.addEventListener('input',handleInput);
 }
 
-const processText = (input,ref) => {
-  //start timer if not running already
-  if (!isTimerRunning) runTimer(true);
-  // check for errors and change background
-  ref.slice(0,input.length) !== input 
-    ? inputTextEl.classList.add('error') 
-    : inputTextEl.classList.remove('error');
-  wordsEl.innerHTML = `${countWords(input)} / ${wordsCount}`;
-  wpmEl.innerText = wordsPerMinutes(input).toFixed(1); 
-  const {errors, accuracy} = calcAccuracy(input,ref);
-  accuDetailEl.innerText = `${input.length - errors} / ${input.length}`;
-  accuracyEl.innerText = `${Math.round(accuracy)}%`;
-}
-
-function endGame(text,ref) {
-  messageEl.innerText = "Test Ended"
-  // stop timer
-  runTimer(false)
-  // disable typing
-  inputTextEl.removeEventListener('input',handleInput);
-  const testResultsEl = showErrors(text,ref)
-  inputTextEl.remove();
-  document.getElementById('texts').appendChild(testResultsEl)
-  inputTextEl.innerHTML = result;
-
-}
-
-const handleInput = (ev) => {
-  ev.preventDefault();
-  const inputText = ev.target.value
-  processText(inputText,currentText);
-  if (inputText.length === currentText.length) endGame(inputText, currentText);
-}
-
-const currentText = selectText();
-const wordsCount = countWords(currentText);
-let timeCounter = 0;
-let isTimerRunning = 0;
-// showWordsPerMinutes("")
-const timerEl = document.getElementById("timer");
-const wordsEl = document.getElementById("words");
-const messageEl = document.getElementById("message");
-const wpmEl = document.getElementById("wpm");
-const accuracyEl = document.getElementById("accuracy");
-const accuDetailEl = document.getElementById("accuracy-detail");
-const inputTextEl = document.getElementById("input");
-
-timerEl.innerText = secondsToString(timeCounter)
-wordsEl.innerText = `0 / ${wordsCount}`;
-accuDetailEl.innerText = `0 / ${wordsCount}`;
-wpmEl.innerText = '0.0';
-accuracyEl.innerText = '100%'
-messageEl.innerText = 'Start typing to begin';
-document.getElementById("test-text").innerText = currentText;
-
-inputTextEl.addEventListener('input',handleInput);
+runTest();
